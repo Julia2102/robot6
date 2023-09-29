1afaf8b87eb68a42d53f9a693fb84810f74ade70
yulia <boykova.julia@bk.ru>
2023-09-29 20:13:59 +0300

    Add change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world my conflict!");
         System.out.println("Hello world!");
     }
 }
